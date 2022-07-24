f74985bddf1c105ad45c618eaa08220b4cf78ccd
Adityansyah <adityansya@gmail.com>
2022-07-24 09:19:48 +0700

    crud

--- app/Http/Controllers/CRUDController.php
+++ app/Http/Controllers/CRUDController.php
@@ -1,10 +1,103 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
 class CRUDController extends Controller
 {
-    //
+    public function index(){
+        return view("crud",[
+            "posts"=>Post::latest()->get()
+        ]);
+
+    }  
+    public function post(Request $req)
+    {
+        $this->validate($req, [
+            'title' => 'required|string|max:155',
+            'content' => 'required',
+            'status' => 'required'
+        ]);
+
+        $post = Post::create([
+            'title' => $req->title,
+            'content' => $req->content,
+            'status' => $req->status,
+            'slug' => Str::slug($req->title)
+        ]);
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'New post has been created successfully'
+                ]);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem occurred, please try again'
+                ]);
+        }
+    }
+    public function create(){
+        return view("createnewpost");
+        
+    }
+    public function destroy(Request $req,$id){
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'Post has been deleted successfully'
+                ]);
+        } else {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'error' => 'Some problem has occurred, please try again'
+                ]);
+        }
+    }
+    public function edit($id){
+        $post= Post::findOrFail($id);
+        return view ("edit",compact("post"));
+    }
+    public function update(Request $req, $id){
+        $this->validate($req, [
+            'title' => 'required|string|max:155',
+            'content' => 'required',
+            'status' => 'required'
+        ]);
+
+        $post = Post::findOrFail($id);
+
+        $post->update([
+            'title' => $req->title,
+            'content' => $req->content,
+            'status' => $req->status,
+            'slug' => Str::slug($req->title)
+        ]);
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'Post has been updated successfully'
+                ]);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem has occured, please try again'
+                ]);
+        }
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+use App\Http\Controllers\CRUDController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('crud', [CRUDController::class,"index"])->name("post.index");
+Route::post('crud',[CRUDController::class,"post"])->name("post.store");
+Route::get('create', [CRUDController::class,"create"]);
+Route::delete('destroy/{id}', [CRUDController::class,"destroy"])->name("post.destroy");
+Route::get('edit/{id}',[CRUDController::class,"edit"])->name("post.edit");
+Route::put('update/{id}',[CRUDController::class,"update"])->name("post.update");
