96da3f7338a6974cf4bee68c6c30324fee7693c3
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-09-04 09:09:34 +0700

    menambah template

---
+++ resources/views/home/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- App css -->
+    <link href="template/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="template/assets/css/app.min.css" rel="stylesheet" type="text/css" id="light-style" />
+    <link href="template/assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style" />
+</head>
+
+<body>
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CRUDController;
+
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('crud', [CRUDController::class,"index"])->name("post.index");
-Route::post('crud',[CRUDController::class,"post"])->name("post.store");
-Route::get('create', [CRUDController::class,"create"]);
-Route::delete('destroy/{id}', [CRUDController::class,"destroy"])->name("post.destroy");
-Route::get('edit/{id}',[CRUDController::class,"edit"])->name("post.edit");
-Route::put('update/{id}',[CRUDController::class,"update"])->name("post.update");
+Route::get('crud', [CRUDController::class, "index"])->name("post.index");
+Route::post('crud', [CRUDController::class, "post"])->name("post.store");
+Route::get('create', [CRUDController::class, "create"]);
+Route::delete('destroy/{id}', [CRUDController::class, "destroy"])->name("post.destroy");
+Route::get('edit/{id}', [CRUDController::class, "edit"])->name("post.edit");
+Route::put('update/{id}', [CRUDController::class, "update"])->name("post.update");
+
+Route::view('dashboard', 'home.dashboard')->name("dashboard");
+Route::view('login', 'auth.login')->name("login");
